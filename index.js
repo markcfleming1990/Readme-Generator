cd4f8bed054442a9228096d03d2b113c0a3ea922
Mark <markcfleming1990@gmail.com>
2022-02-04 15:49:07 -0600

    worked on user input

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
-// TODO: Include packages needed for this application
-
+//packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
+const util = require('util');
+//const generateReadme = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the project title?(Reqired)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter a Project Title!');
+          return false;
+        }
+      }
+    },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a brief description of your project: "
+    },
+  {
+    type: "input",
+    name: "installation",
+    message: "Describe the installation process if any: ",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is this project usage for?"
+    },
+  {
+    type: "list",
+    name: "license",
+    message: "Chose the appropriate license for this project: ",
+    choices: [
+        "MIT",
+    "APACHE 2.0",
+    "GPL 3.0",
+    "BSD 3.0",
+    "NONE",
+      ]
+    },
+{
+  type: "input",
+    name: "contributing",
+      message: "Who are the contributors of this projects?"
+},
+{
+  type: "input",
+    name: "tests",
+      message: "Is there a test included?"
+},
+{
+  type: "input",
+    name: "questions",
+      message: "What do I do if I have an issue? "
+},
+{
+  type: "input",
+    name: "username",
+      message: "Please enter your GitHub username: "
+},
+{
+  type: "input",
+    name: "email",
+      message: "Please enter your email: "
+}
+  ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+promptUser();
