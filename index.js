762f558051efc6719d75e1b1f8e362f994d7bcf0
Mark <markcfleming1990@gmail.com>
2022-02-05 08:59:13 -0600

    added validate and .then function

--- index.js
+++ index.js
@@ -19,59 +19,111 @@ const promptUser = () => {
         }
       }
     },
-  {
-    type: "input",
-    name: "description",
-    message: "Write a brief description of your project: "
-    },
-  {
-    type: "input",
-    name: "installation",
-    message: "Describe the installation process if any: ",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What is this project usage for?"
-    },
-  {
-    type: "list",
-    name: "license",
-    message: "Chose the appropriate license for this project: ",
-    choices: [
-        "MIT",
-    "APACHE 2.0",
-    "GPL 3.0",
-    "BSD 3.0",
-    "NONE",
-      ]
-    },
-{
-  type: "input",
-    name: "contributing",
-      message: "Who are the contributors of this projects?"
-},
-{
-  type: "input",
-    name: "tests",
-      message: "Is there a test included?"
-},
-{
-  type: "input",
-    name: "questions",
-      message: "What do I do if I have an issue? "
-},
-{
-  type: "input",
-    name: "username",
-      message: "Please enter your GitHub username: "
-},
-{
-  type: "input",
-    name: "email",
-      message: "Please enter your email: "
-}
   ]);
 };
 
-promptUser();
+const promptProject = portfolioData => {
+  console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: ",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter a description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any: ",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is this project usage for?"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+          "MIT",
+          "APACHE 2.0",
+          "GPL 3.0",
+          "BSD 3.0",
+          "NONE",
+        ],
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please select a license!');
+            return false;
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this projects?"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included?"
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? "
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: ",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: ",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your email!');
+            return false;
+          }
+        }
+      }
+    ]);
+};
+
+promptUser()
+.then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
